788c2447670758fc88ac525d627b99a58794233a
Isyaau Akhsanil Khakim <51592091+isyaau@users.noreply.github.com>
2023-01-20 11:58:03 +0700

    upload sistem

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Dashboard extends Controller
+{
+    public function index()
+    {
+        $session = session();
+        echo "Welcome back, " . $session->get('user_name');
+    }
+}
