02a9f32945d4e835f902814cdbdb7d5f7b4960d4
Isyaau <isak.akhsanil@gmail.com>
2023-03-29 12:55:32 +0700

    first commit

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/assets/css/style.css">
 
-    <title>Rental Mobil</title>
+    <title>E-Arsip</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url(); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
